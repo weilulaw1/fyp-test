e88141e3742826c34ce3e72bd263ee30e7b507b6
Wei lu Law <weilulaw@Weis-MacBook-Air.local>
2025-09-10 00:06:33 +0800

    i fucking did it im the fucking goat able to upload zip and other files

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .views import upload_file
 
 urlpatterns = [
-    path('api/home', views.home_data),
+    path('api/home/', views.home_data),
 
     path('api/file/new/', views.file_new),
     path('api/file/open/', views.file_open),
@@ -14,6 +14,6 @@ urlpatterns = [
     
     path("api/view/toggle_sidebar/", views.toggle_sidebar, name="toggle_sidebar"),
 
-    path('api/file/upload', upload_file, name='file-upload'),
+    path('api/file/upload/', upload_file, name="file-upload"),
 
 ]
