f9b37e6ab8b9319586a54338ef509c564b55c084
Weilulaw <weilulaw1@hotmail.sg>
2025-09-22 18:35:08 +0800

    toggle for pdf and svg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,18 @@ import Sidebar from './SideBar';
 import PlantUMLViewer from './plantUML';
 import PlantUMLTest from './plantUMLtest'
 import PlantUMLTestSVG from './plantUMLsvgTest';
+import PlantUMLpdfTest from './plantUMLpdfTest';
 
 function App() {
     // State to control sidebar visibility
     const [sidebarOpen, setSidebarOpen] = useState(false);
     const [uploadedFiles, setUploadedFiles] = useState([]);
+    const [viewMode, setViewMode] = useState("pdf");
+
     //Fucntion to Toggle sidebar, will be passed to menubar
     const toggleSidebar = () => setSidebarOpen((prev) => !prev);
 
+    
   return (
     <div>
        {/* Sidebar */}
@@ -31,12 +35,30 @@ function App() {
         style={{
           marginLeft: sidebarOpen ? "250px" : "0",
           padding: "20px",
+          paddingTop: "80px",
           transition: "margin-left 0.3s ease",
+          minHeight: "100vh",
         }}
       >
+        {/*Switch Buttons*/}
+        <div style={{ marginBottom: "20px" }}>
+          <button 
+            onClick={() => setViewMode("pdf")} 
+            style={{ marginRight: "10px" }}
+          >
+            PDF View
+          </button>
+          <button onClick={() => setViewMode("svg")}>
+            SVG View
+          </button>
+        </div>
         
-        <div style={{ maxWidth: "800px", marginTop: "20px", position:"absolute",top:"40px" }}>
-         <PlantUMLTestSVG/>
+        <div style={{ 
+          maxWidth: "1000px",
+          width:"100%",
+          margin: "0 auto",
+          }}>
+         {viewMode === "pdf"? <PlantUMLpdfTest/>:<PlantUMLTestSVG/>}
         </div>      </div>
     </div>
   );
