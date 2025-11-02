44c104e08e83be871a31b849d96a1713d8848cdb
Weilulaw <weilulaw1@hotmail.sg>
2025-11-02 14:40:31 +0800

    move files back to sidebar and button to run json to uml works

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,18 @@ import './App.css';
 import { useState } from "react";
 import MenuBar from './MenuBar';
 import Sidebar from './SideBar';
-import DiagramView from './DiagramView'; 
+import DiagramView from './DiagramView';
 import PlantUMLTestSVG from './plantUMLsvg';
 import PlantUMLpdfTest from './plantUMLpdf';
 import CodeViewer from './CodeViewer';
-import FolderView from './FolderView'; 
+
 function App() {
-  // Sidebar + view control
   const [sidebarOpen, setSidebarOpen] = useState(true);
   const [uploadedFiles, setUploadedFiles] = useState([]);
   const [activeFile, setActiveFile] = useState(null);
-  const [activeSection, setActiveSection] = useState("folder");
 
-  // Toggle sidebar visibility
   const toggleSidebar = () => setSidebarOpen((prev) => !prev);
 
-  // Components for each diagram mode
   const modeComponents = {
     SVG: <PlantUMLTestSVG file={activeFile} />,
     PDF: <PlantUMLpdfTest file={activeFile} />,
@@ -29,23 +25,24 @@ function App() {
       {/* Sidebar */}
       <Sidebar
         isOpen={sidebarOpen}
-        uploadedFiles={uploadedFiles}
         toggleSidebar={toggleSidebar}
+        uploadedFiles={uploadedFiles}
         onFileClick={(filename) => {
           const relativePath = filename.replaceAll("\\", "/");
           setActiveFile(relativePath);
         }}
-        onSectionChange={setActiveSection}
+        selectedFile={activeFile}
+        setSelectedFile={setActiveFile}
       />
 
-      {/* MenuBar */}
+      {/* Menu bar */}
       <MenuBar
         onToggleSidebar={toggleSidebar}
         sidebarOpen={sidebarOpen}
         setUploadedFiles={setUploadedFiles}
       />
 
-      {/* Main content area */}
+      {/* Main content */}
       <div
         style={{
           padding: "20px",
@@ -55,16 +52,7 @@ function App() {
           marginLeft: sidebarOpen ? "250px" : "52px",
         }}
       >
-        {activeSection === "diagram" ? (
-          <DiagramView modeComponents={modeComponents} />
-        ) : (
-          <FolderView
-            onFileClick={(file) => setActiveFile(file)}
-            uploadedFiles={uploadedFiles}
-            selectedFile={activeFile}
-            setSelectedFile={setActiveFile}
-          />
-        )}
+        <DiagramView modeComponents={modeComponents} />
       </div>
     </div>
   );
